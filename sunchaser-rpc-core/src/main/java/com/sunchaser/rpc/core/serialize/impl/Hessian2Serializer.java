30e43e621075629f6f2f3d7c2ce31cdf92d55647
lilu <admin@lilu.org.cn>
2022-07-15 18:33:37 +0800

    [feat] add comment

--- sunchaser-rpc-core/src/main/java/com/sunchaser/rpc/core/serialize/impl/Hessian2Serializer.java
+++ sunchaser-rpc-core/src/main/java/com/sunchaser/rpc/core/serialize/impl/Hessian2Serializer.java
@@ -17,6 +17,12 @@ import java.util.Objects;
  */
 public class Hessian2Serializer implements Serializer {
 
+    /**
+     * 将对象进行序列化
+     *
+     * @param obj 待序列化的对象
+     * @return 序列化后的byte字节数组
+     */
     @SneakyThrows
     @Override
     public <T> byte[] serialize(T obj) {
@@ -33,6 +39,13 @@ public class Hessian2Serializer implements Serializer {
         }
     }
 
+    /**
+     * 将二进制字节数组进行反序列化
+     *
+     * @param data  二进制字节数组
+     * @param clazz 待反序列化的class类型
+     * @return 反序列化后的对象
+     */
     @SneakyThrows
     @Override
     @SuppressWarnings("unchecked")
--- sunchaser-rpc-core/src/main/java/com/sunchaser/rpc/core/serialize/impl/XmlSerializer.java
+++ sunchaser-rpc-core/src/main/java/com/sunchaser/rpc/core/serialize/impl/XmlSerializer.java
@@ -10,11 +10,24 @@ import com.sunchaser.rpc.core.serialize.Serializer;
  */
 public class XmlSerializer implements Serializer {
 
+    /**
+     * 将对象进行序列化
+     *
+     * @param obj 待序列化的对象
+     * @return 序列化后的byte字节数组
+     */
     @Override
     public <T> byte[] serialize(T obj) {
         return new byte[0];
     }
 
+    /**
+     * 将二进制字节数组进行反序列化
+     *
+     * @param data  二进制字节数组
+     * @param clazz 待反序列化的class类型
+     * @return 反序列化后的对象
+     */
     @Override
     public <T> T deserialize(byte[] data, Class<T> clazz) {
         return null;
