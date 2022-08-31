7e936b36125f6e00d911f48cc02e775c57d4cdad
lilu <admin@lilu.org.cn>
2022-08-31 23:37:40 +0800

    [feat] remove useless imports

--- sunchaser-rpc-core/src/main/java/com/sunchaser/shushan/rpc/core/balancer/Node.java
+++ sunchaser-rpc-core/src/main/java/com/sunchaser/shushan/rpc/core/balancer/Node.java
@@ -1,7 +1,6 @@
 package com.sunchaser.shushan.rpc.core.balancer;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
